763351938ce1aecb27c8b5a9e3aab6edf88609e0
firmanazhary <azharyoke948@gmail.com>
2026-01-22 15:32:25 +0700

    packaging & formulir

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PackageSelection from "./components/home/package/Package";
+import OrderForm from "./components/formulir/Formulir";
 
 function App() {
   return (
-    <>
-      <h1 className="text-[100px]">Ticketing App</h1>
-    </>
+
+    <Router basename="/ticketing">
+     
+        <Routes>
+          
+          <Route 
+            path="/" 
+            element={
+              <>
+                <PackageSelection />
+              </>
+            } 
+          />
+
+          <Route 
+            path="/order" 
+            element={
+              <OrderForm />
+            } 
+          />
+          
+        </Routes>
+   
+    </Router>
   );
 }
 
